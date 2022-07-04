03cadaff777d0b2d2e96fbee7a34055186b90fb8
SparksNirm <panchalnirmal157@gmail.com>
2022-07-04 15:13:48 -0400

    Editing server.js file for api

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const router = jsonServer.router("./db.json");
 const middleware = jsonServer.defaults({ static: "./build" });
 const port = process.env.PORT || 8000;
 server.use(middleware);
-server.use(router);
 
+server.use(jsonServer.rewriter({
+  '/api/*':'$1',
+}))
+server.use(router);
 server.listen(port, () =>
   console.log(`Successfully started server on port: ${port}`)
 );
